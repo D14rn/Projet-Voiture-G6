6c8754bacaf901a3eafa974cc92ce9f5510d8282
D14rn <49399494+D14rn@users.noreply.github.com>
2024-04-16 11:42:14 +0200

    [Feat] Progress in tests

--- tests/color_sensor.test.py
+++ tests/color_sensor.test.py
@@ -1,24 +1,36 @@
 import unittest
-from src.sensors import ColorSensor
 import time
+from src.sensors import ColorSensor
 
 
 class ColorSensorTest(unittest.TestCase):
-
-    @staticmethod
-    def test_getValue():
+    def test_getValue(self):
         color_sensor = ColorSensor("Color sensor", 5)
-        for i in range(30):
-            print(color_sensor.get_value())
-            time.sleep(0.5)
+        rgb_value = color_sensor.get_value()
+        self.assertIsInstance(rgb_value, tuple)
+        self.assertEqual(len(rgb_value), 3)
 
-    @staticmethod
-    def test_isGreen():
+    def test_isGreen(self):
         color_sensor = ColorSensor("Color sensor", 5)
-        for i in range(30):
-            print(color_sensor.is_green())
-            time.sleep(0.5)
+        res = color_sensor.is_green()
+        self.assertIsInstance(res, bool)
+
+def test_long_get_value():
+    color_sensor = ColorSensor("Color sensor", 5)
+    print("Testing the color sensor value: rgb tuple")
+    for _ in range(10):
+        print(color_sensor.get_value())
+        time.sleep(0.5)
+
+def test_long_is_green():
+    color_sensor = ColorSensor("Color sensor", 5)
+    print("Testing green color, try putting some green stuff in front!")
+    for _ in range(10):
+        print(color_sensor.is_green())
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
     unittest.main()
+    test_long_get_value()
+    test_long_is_green()
--- tests/light_sensor_test.py
+++ tests/light_sensor_test.py
@@ -1,9 +1,22 @@
-if __name__ == "__main__":
-    from src.sensors import LightSensor
-    from time import sleep
+import unittest
+import time
+from src.sensors import LightSensor
 
 
-    light_sensor = LightSensor("light sensor", 20)
-    for i in range (15):
+class LightSensorTest(unittest.TestCase):
+    def test_get_value(self):
+        test_sensor = LightSensor("test light sensor", 20)
+        correct_values = (0, 1)
+        test_value = test_sensor.get_value()
+        self.assertTrue(test_value in correct_values)
+
+def test_long_lightsensor_get_value():
+    light_sensor = LightSensor("test light sensor", 20)
+    for _ in range (10):
         print(light_sensor.get_value())
-        sleep(0.5)
+        time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    unittest.main()
+    test_long_lightsensor_get_value()
