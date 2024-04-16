ca923861f70d88f7b6f646811c0c2c36cd4cda40
D14rn <49399494+D14rn@users.noreply.github.com>
2024-04-16 11:01:18 +0200

    [Feat] Add gpio cleanup exit handler test

--- tests/distance_sensor_test.py
+++ tests/distance_sensor_test.py
@@ -1,17 +1,21 @@
+from time import sleep
+from src.sensors import DistanceSensor
+from src.lib import exit_handler
+
+
 if __name__ == "__main__":
-    from time import sleep
-    from src.sensors import DistanceSensor
-    from src.config import DISTANCE_SENSORS_BOARD_PINS as dsbp
-
     sleep_duration = 1
-    front_sensor = DistanceSensor("front sensor", dsbp["FRONT"]["TRIGGER"], dsbp["FRONT"]["ECHO"])
-    right_sensor = DistanceSensor("right sensor", dsbp["RIGHT"]["TRIGGER"], dsbp["RIGHT"]["ECHO"])
-    left_sensor = DistanceSensor("left sensor", dsbp["LEFT"]["TRIGGER"], dsbp["LEFT"]["ECHO"])
+    front_sensor = DistanceSensor("front sensor", 6, 5)
+    left_sensor = DistanceSensor("left sensor", 11, 9)
+    right_sensor = DistanceSensor("right sensor", 26, 19)
 
-    sensors = [left_sensor]
-
+    sensors = [front_sensor, left_sensor, right_sensor]
+    distance_mesures = []
+    tests_count = 5
     for sensor in sensors:
-        print(f"Testing sensor: {sensor.s_name}")
-        for _ in range(5):
-            print(sensor.get_value())
+        print(f"Testing {sensor.s_name}")
+        for _ in range(tests_count):
+            current_value = sensor.get_value()
+            print(current_value)
+            distance_mesures.append(current_value)
             sleep(sleep_duration)
---
+++ tests/gpio_cleanup_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class CleanupTest(unittest.TestCase):
+    def test_atexit_register(self):
+        import atexit
+
+        before_import_count = atexit._ncallbacks()
+
+        from src.lib import exit_handler
+
+        after_import_count = atexit._ncallbacks()
+
+        self.assertEqual(before_import_count + 1, after_import_count)
+
+        exit_handler()
+
+        self.assertEqual(before_import_count + 1, after_import_count)
+
+
+if __name__ == "__main__":
+    unittest.main()
