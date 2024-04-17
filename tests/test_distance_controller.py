d082f7d0e37cb865fac3bb6e1a2854fd06480207
Franck <franckwanji@gmail.com>
2024-04-17 15:50:45 +0200

    edit
    modif test_distance_controller et ajout test_mouvement_controller

--- tests/test_distance_controller.py
+++ tests/test_distance_controller.py
@@ -1,14 +1,17 @@
-import unittest, time
+import unittest
 from src.controllers import DistanceController
+from src.sensors import DistanceSensor
 
 class DistanceControllerTest(unittest.TestCase):
     def setUp(self) -> None:
         self.front_sensor = DistanceSensor("front sensor", 6, 5)
         self.left_sensor = DistanceSensor("left sensor", 11, 9)
         self.right_sensor = DistanceSensor("right sensor", 26, 19)
-        
-        self.distController = DistanceController(self.front_sensor, self.left_sensor, self.right_sensor)
-        
+        self.distController = DistanceController(self.front_sensor,self.left_sensor,self.right_sensor)
+
+    def tearDown(self)-> None:
+        del self.distController
+
     def test_start(self):
         self.assertIsNotNone(self.distController)
         self.assertIsInstance(self.distController, DistanceController)
@@ -19,18 +22,20 @@ class DistanceControllerTest(unittest.TestCase):
         
     def test_front_distance(self):
         # Simulation d'une valeur de capteur
-        self.front_sensor.value = 50
-        self.assertEqual(self.distController.front_distance(),30)
+        self.front_sensor.value = 30.123
+        self.assertAlmostEqual(self.distController.front_distance(),30.123,2)
 
     def test_left_distance(self):
-        self.front_sensor.value = 15
-        self.assertEqual(self.distController.front_distance(),15)
-        
+        self.left_sensor.value = 15.678
+        self.assertAlmostEqual(self.distController.left_distance(),15.678,2)
+
     def test_right_distance(self):
-        self.front_sensor.value = 40
-        self.assertEqual(self.distController.front_distance(),40)
+        self.right_sensor.value = 40.987
+        self.assertAlmostEqual(self.distController.right_distance(),40.987,2)
 
     def test_stop(self):
+        self.assertIsNotNone(self.distController)
+        self.assertIsInstance(self.distController, DistanceController)
         self.distController.stop()
         self.assertTrue(self.front_sensor.deactivate())
         self.assertTrue(self.left_sensor.deactivate())
