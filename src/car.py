85305c76aa7992f2bb9f48e9e9f554473c8b5e33
Ioka <clearseasc@gmail.com>
2024-04-17 20:13:11 +0200

    Follow wall

--- src/car.py
+++ src/car.py
@@ -1,55 +1,71 @@
-import time
+"""
+'Samir you're breaking the car!'
+- Vivek Ponnusamy
+
+https://www.youtube.com/watch?v=D9-voINFkCg
+"""
+import time as t
 
 
 class Car():
     """
     Objet voiture composé de contrôleurs et possédant des méthodes pour les modes : autonome et contrôlé
     """
-
     def __init__(self, movement_controller, distance_controller, state_controller):
-        super().__init__()
         self.__movement_controller = movement_controller
         self.__distance_controller = distance_controller
         self.__state_controller = state_controller
 
+    @property
+    def samir(self): # Appelle le pilote
+        """
+        Contrôleur de mouvement de la voiture
+        """
+        return self.__movement_controller
+    
+    @property
+    def vivek(self): # Appelle le co-pilote
+        """
+        Contrôleur de distance de la voiture
+        """
+        return self.__distance_controller
+
     def __str__(self) -> str:
         return str(self.__dict__)
 
-    def follow_wall(self):
-        self.__distance_controller.start()
-        while self.__distance_controller.front_distance < 10:
-            if self.__distance_controller.front_distance < 15:
-                self.__movement_controller.go_backward()
-                time.sleep(0.5)
-                self.__movement_controller.stop()
-                break
-
-            if self.__distance_controller.right_distance > 20:
-                self.__movement_controller.turn_right()
-                time.sleep(0.3)
-                self.__movement_controller.reset_direction()
-
-            if self.__distance_controller.right_distance < 15:
-                self.__movement_controller.turn_left()
-                time.sleep(0.3)
-                self.__movement_controller.reset_direction()
+    def follow_right_wall(self, duration: int = 10) -> None:
+        """
+        Fais avancer la voiture à une distance fixe d'un mur durant durée déterminée
+        """
+        self.vivek.start()
+        self.samir.go_forward()
+        start_time = t.time()
+        while (t.time() - start_time) < duration:
+            if self.vivek.right_distance > 22:
+                self.samir.easy_right()
+            elif self.vivek.right_distance < 17:
+                self.samir.easy_left()
+            else:
+                self.samir.stay_center()
+        self.samir.stop()
+        self.vivek.stop()
 
     def avoid_object(self):
         self.__distance_controller.start()
         while True:
             if self.__distance_controller.front_distance() < 50:
-                start_time = time.time()
-                while (time.time() - start_time) < 4:
+                start_time = t.time()
+                while (t.time() - start_time) < 4:
                     gauche = self.__distance_controller.left_distance()
                     droite = self.__distance_controller.right_distance()
                     if gauche > droite:
-                        self.__movement_controller.turn_left()
-                        time.sleep(0.4)
-                        self.__movement_controller.reset_direction()
+                        self.samir.turn_left()
+                        t.sleep(0.4)
+                        self.samir.reset_direction()
                     else:
-                        self.__movement_controller.turn_right()
-                        time.sleep(0.4)
-                        self.__movement_controller.reset_direction()
+                        self.samir.turn_right()
+                        t.sleep(0.4)
+                        self.samir.reset_direction()
 
     def autonomous_mode(self) -> None:
         """
--- src/controllers/movement_controller.py
+++ src/controllers/movement_controller.py
@@ -1,8 +1,11 @@
-class MovementController:
+from threading import Thread
+
+class MovementController(Thread):
     """
     Permet de contrôler les mouvements de la voiture
     """
     def __init__(self, direction, motor, speed=50) -> None:
+        Thread.__init__(self)
         self.__direction = direction
         self.__motor = motor
         self.__speed = speed
@@ -17,13 +20,46 @@ class MovementController:
     def go_backward(self) -> None:
         self.__motor.move(-self.__speed, backward=True)
 
-    def turn_left(self) -> None:
+    def sharp_left(self) -> None:
+        """
+        Tourne à gauche fort
+        """
         self.__direction.turn_left(45)
 
-    def turn_right(self) -> None:
+    def medium_left(self) -> None:
+        """
+        Tourne à gauche moyen
+        """
+        self.__direction.turn_left(30)
+
+    def easy_left(self) -> None:
+        """
+        Tourne à gauche d'une miette
+        """
+        self.__direction.turn_left(15)
+
+    def sharp_right(self) -> None:
+        """
+        Tourne à droite fort
+        """
         self.__direction.turn_right(45)
+
+    def medium_right(self) -> None:
+        """
+        Tourne à droite moyen
+        """
+        self.__direction.turn_left(30)
+
+    def easy_right(self) -> None:
+        """
+        Tourne à droite d'une miette
+        """
+        self.__direction.turn_left(15)
     
-    def reset_direction(self):
+    def stay_center(self):
+        """
+        Retourne au centre (duh)
+        """
         self.__direction.home()
 
     def stop(self) -> None:
@@ -36,3 +72,6 @@ class MovementController:
     def reset(self):
         self.stop()
         self.reset_direction()
+
+    def run(self) -> None:
+        pass
