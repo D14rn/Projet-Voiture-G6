12880c373f5ed27185f31159bd1652923f2a1344
D14rn <49399494+D14rn@users.noreply.github.com>
2024-04-18 09:52:40 +0200

    [Refactor] Refactor movement controller and direction class

--- src/controllers/movement_controller.py
+++ src/controllers/movement_controller.py
@@ -1,62 +1,77 @@
-from threading import Thread
-
-class MovementController(Thread):
+class MovementController:
     """
     Permet de contrôler les mouvements de la voiture
     """
-    def __init__(self, direction, motor, speed=50) -> None:
-        Thread.__init__(self)
+    def __init__(self, direction, motor, max_speed=70) -> None:
         self.__direction = direction
         self.__motor = motor
-        self.__speed = speed
+        self.__max_speed = max_speed
+        self.__speed = 0
+        self.__acceleration = 5
+
+    @property
+    def max_speed(self) -> int:
+        return self.__max_speed
+
+    @property
+    def speed(self) -> int:
+        return self.__speed
+
+    @speed.setter
+    def speed(self, new_speed: int) -> None:
+        res = min(max(-100, new_speed), 100)
+        self.__speed = res
+        if res >= 0:
+            self.go_forward()
+        else:
+            self.go_backward()
 
     def go_forward(self) -> None:
         self.__motor.move(self.__speed)
-    
-    def change_speed(self, new_speed):
-        res = min(max(0, new_speed), 100)
-        self.__speed = res
 
     def go_backward(self) -> None:
         self.__motor.move(-self.__speed, backward=True)
+
+    def turn_left(self, angle: int) -> None:
+        self.__direction.turn_left(angle)
 
     def sharp_left(self) -> None:
         """
         Tourne à gauche fort
         """
-        self.__direction.turn_left(45)
+        self.turn_left(45)
 
     def medium_left(self) -> None:
         """
         Tourne à gauche moyen
         """
-        self.__direction.turn_left(30)
+        self.turn_left(30)
 
     def easy_left(self) -> None:
         """
         Tourne à gauche d'une miette
         """
-        self.__direction.turn_left(15)
+        self.turn_left(15)
 
     def sharp_right(self) -> None:
         """
         Tourne à droite fort
         """
-        self.__direction.turn_right(45)
+        self.turn_right(45)
 
     def medium_right(self) -> None:
         """
         Tourne à droite moyen
         """
-        self.__direction.turn_left(30)
+        self.turn_right(30)
 
     def easy_right(self) -> None:
         """
         Tourne à droite d'une miette
         """
-        self.__direction.turn_left(15)
+        self.turn_right(15)
     
-    def stay_center(self):
+    def stay_center(self) -> None:
         """
         Retourne au centre (duh)
         """
@@ -65,13 +80,12 @@ class MovementController(Thread):
     def stop(self) -> None:
         self.__motor.stop()
 
-    def accelerate(self):
-        self.__speed += 10
-        self.__motor.move()
+    def accelerate(self) -> None:
+        self.__speed += self.__acceleration
     
-    def reset(self):
+    def decelerate(self) -> None:
+        self.__speed -= self.__acceleration
+    
+    def reset(self) -> None:
         self.stop()
-        self.reset_direction()
-
-    def run(self) -> None:
-        pass
+        self.stay_center()
